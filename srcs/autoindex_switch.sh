dfd5930171a2c665584a3ad2727f6e2c432771eb
Sarah LESCURE <slescure@e2r7p2.42.fr>
2021-01-21 15:53:07 +0100

    autoindex tout bon

--- srcs/autoindex_switch.sh
+++ srcs/autoindex_switch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if grep "autoindex on;" /etc/nginx/sites-enabled/localhost > /dev/null; then
+if grep "autoindex on;" /etc/nginx/sites-enabled/nginx-config > /dev/null; then
   echo "autoindex is on, turning it off"
-  sed -i 's/autoindex on/autoindex off/' /etc/nginx/sites-available/localhost
-  sed -i 's/root \/var\/www\//root \/var\/www\/localhost\//' /etc/nginx/sites-available/localhost
+  sed -i 's/autoindex on/autoindex off/' /etc/nginx/sites-available/nginx-config
+  sed -i 's/root \/var\/www\//root \/var\/www\/nginx-config\//' /etc/nginx/sites-available/nginx-config
 else
   echo "autoindex is off, turning it on"
-  sed -i 's/autoindex off/autoindex on/' /etc/nginx/sites-available/localhost
-  sed -i 's/root \/var\/www\/localhost\//root \/var\/www\//' /etc/nginx/sites-available/localhost
+  sed -i 's/autoindex off/autoindex on/' /etc/nginx/sites-available/nginx-config
+  sed -i 's/root \/var\/www\/nginx-config\//root \/var\/www\//' /etc/nginx/sites-available/nginx-config
 fi
 nginx -s reload
